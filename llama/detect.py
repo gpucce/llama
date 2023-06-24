7917a28f6a5e7a738dea4a528147b36f3728784d
Giovanni Puccetti <g.puccetti92@gmail.com>
2023-06-24 10:28:46 +0200

    small changes

--- llama/detect.py
+++ llama/detect.py
@@ -3,17 +3,13 @@ import numpy as np
 
 def compute_prob(x, axis=None):
     return np.sum(np.log(x), axis=axis) / len(x)
-    # return np.log(
-    #     np.prod(x, axis=axis) ** (1/len(x)),
-    # )
 
 
 def detect_gpt(df, true_col):
-    df_true = df.loc[:, true].apply(compute_prob).values
+    df_true = df.loc[:, true_col].apply(compute_prob).values
     df_syn = (
-        df.loc[:, [i for i in df.columns if i != true]].applymap(compute_prob).values
+        df.loc[:, [i for i in df.columns if i != true_col]].applymap(compute_prob).values
     )
-    # assert df_syn.shape[1] == 100
 
     mean_df_syn = np.mean(df_syn, axis=1)  # / df_syn.shape[1]
     unnorm_denom = df_syn - mean_df_syn.reshape(-1, 1)
